419bfda53f7d16350b967533359c22aeb4a18e8c
Edward Zhang <edwardz@princeton.edu>
2012-04-26 19:13:28 -0400

    Implemented basic interaction

--- interaction.js
+++ interaction.js
@@ -1,40 +1,48 @@
 VoronoiSystem = function(thecanvas) {
     var points = [];
     var linepos = 0;
-    var canvas = thecanvas;
+    var canvas = thecanvas.get(0);
     var ctx = canvas.getContext("2d");
-    var w = canvas.height();
-    var h = canvas.width();
+    var w = thecanvas.height();
+    var h = thecanvas.width();
     var maxx = Number.NEGATIVE_INFINITY;
     var maxy = Number.NEGATIVE_INFINITY;
-    var minx = Number.POSITIVE_INFINTY;
-    var miny = Number.POSITIVE_INFINTY;
+    var minx = Number.POSITIVE_INFINITY;
+    var miny = Number.POSITIVE_INFINITY;
     bounds = [0,0,1,1];
 
     var that = {
         addHandlers:function() {
-            canvas.click(function(e) {
-                that.addPoint(that.fromScreen(e.pageX, e.pageY));
+            $(window).resize(that.resize);
+            linepos = bounds[0];
+            that.resize();
+            that.canvasBounds();
+            thecanvas.click(function(e) {
+                var x = e.pageX - canvas.offsetLeft;
+                var y = e.pageY - canvas.offsetTop;
+                var tmp = that.fromScreen(x, y);
+                that.addPoint(that.fromScreen(x, y));
+                linepos = bounds[0];
                 that.update();
             });
         },
         addPoint:function(x, y) {
-            if (y === undefined) {
+            if (typeof(x) == 'object') {
                 y = x.y;
                 x = x.x;
             }
             points.push({x:x, y:y});
             if (x > maxx) maxx = x;
             if (y > maxy) maxy = y;
-            if (x > minx) minx = x;
-            if (y > miny) miny = y;
+            if (x < minx) minx = x;
+            if (y < miny) miny = y;
         },
         refreshBounds:function() {
             for (var i = 0; i < points.length; ++i) {
                 if (points[i].x > maxx) maxx = points[i].x;
                 if (points[i].y > maxy) maxy = points[i].y;
-                if (points[i].x > minx) minx = points[i].x;
-                if (points[i].y > miny) miny = points[i].y;
+                if (points[i].x < minx) minx = points[i].x;
+                if (points[i].y < miny) miny = points[i].y;
             }
         },
         canvasBounds:function() {
@@ -56,13 +64,20 @@ VoronoiSystem = function(thecanvas) {
                 var diff = expected - (maxy-miny);
                 bounds = [minx, miny-diff/2, maxx, maxy+diff/2];
             }
+            bounds[0] -= 0.05*(bounds[2]-bounds[0]);
+            bounds[1] -= 0.05*(bounds[3]-bounds[1]);
+            bounds[2] += 0.05*(bounds[2]-bounds[0]);
+            bounds[3] += 0.05*(bounds[3]-bounds[1]);
         },
         resize:function() {
             w = $(window).width()*0.8;
-            h = $(window).height()*0.6;
-            canvas.width(w);
-            canvas.height(h);
-            that.canvasBounds();
+            h = $(window).height()*0.8;
+            w = Math.min(w,h);
+            h = w;
+            thecanvas.width(w);
+            thecanvas.height(h);
+            canvas.width = w;
+            canvas.height = h;
         },
         toScreen:function(x,y){
             if (y === undefined) {
@@ -85,21 +100,23 @@ VoronoiSystem = function(thecanvas) {
             return {x:xx, y:yy};
         },
         update:function() {
+            //that.canvasBounds();
             ctx.clearRect(0,0,w,h);
             // Draw points
             for (var i = 0; i < points.length; ++i) {
                 var coords = that.toScreen(points[i]);
-                ctx.fillStyle("#000000");
+                ctx.fillStyle = "#000000";
                 ctx.beginPath();
-                ctx.arc(coords.x, coords.y, 2, 0, Math.PI*2, true);
+                ctx.arc(coords.x, coords.y, 5, 0, Math.PI*2, true);
                 ctx.closePath();
                 ctx.fill();
             }
             // Draw sweep line
-            ctx.strokeStyle("#ff0000");
+            var l = that.toScreen(linepos, 0);
+            ctx.strokeStyle = "#ff0000";
             ctx.beginPath();
-            ctx.moveTo(0,linepos);
-            ctx.lineTo(h,linepos);
+            ctx.moveTo(l.x, 0);
+            ctx.lineTo(l.x, h);
             ctx.closePath();
             ctx.stroke();
             // TODO: Draw voronoi lines
